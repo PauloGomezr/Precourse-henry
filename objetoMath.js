cf59ff162615331f4071fbe1694e44ecedcb86c9
Paulo Gomez <paulogomezr@gmail.com>
2023-03-23 09:36:13 -0500

    Commit M04

--- objetoMath.js
+++ objetoMath.js
@@ -1,4 +1,2 @@
-const match = require("nodemon/lib/monitor/match");
-
 Math.max(4,5,8,9,1,2,3,7);
 
---
+++ operadoresLogicos.js
@@ -0,0 +1,7 @@
+function mayorYMenor(num) {
+    if(num > 5 && num < 10 )console.log(true);
+    else console.log(false);
+};
+
+mayorYMenor(2);
+mayorYMenor(7);
